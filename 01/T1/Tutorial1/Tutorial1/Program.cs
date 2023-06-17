ecd84e567f479a9f226dcd540eec5e3269547cbc
hugoBustamanteRamirez <Hugobus2001@gmail.com>
2023-06-17 04:36:10 -0600

    creacion de la carpeta t1

---
+++ 01/T1/Tutorial1/Tutorial1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AudioManager;
+namespace Tutorial1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+            Animal animal=new Animal();
+            animal.animalSound();
+           Console.ReadKey();
+        }
+    }
+}
